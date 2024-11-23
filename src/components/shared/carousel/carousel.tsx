ac56a7d1d0520df2ac498447290f9e691141a6b1
Abhinav <gargabhinav0001@gmail.com>
2024-11-23 12:22:02 +0530

    feat: styling carousel

--- src/components/shared/carousel/carousel.tsx
+++ src/components/shared/carousel/carousel.tsx
@@ -15,35 +15,28 @@ import "@/components/shared/carousel/carousel.css";
 export function SharedCarousel({ delay = 2000 }: { delay?: number }) {
   return (
     <Carousel
-      className="w-full carouselContainer"
-      plugins={[
-        Autoplay({
-          delay: delay,
-        }),
-      ]}
-      opts={{
-        align: "start",
-        loop: true,
-      }}
+      className="mt-[2rem]"
+      opts={{ loop: true }}
+      plugins={[Autoplay({ delay: delay })]}
     >
-      <CarouselContent className="-ml-1 carouselContent">
+      <CarouselContent>
         {Array.from({ length: 5 }).map((_, index) => (
           <CarouselItem
             key={index}
-            className="pl-1 md:basis-1/2 lg:basis-1/3 carouselItem"
+            className="flex justify-center items-center"
           >
-            <div className="p-1">
-              <Card>
-                <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <span className="text-2xl font-semibold">{index + 1}</span>
+            <div className="p-1 w-[1200px]">
+              <Card className="h-[400px]">
+                <CardContent className="flex items-center justify-center p-6">
+                  <span className="text-4xl font-semibold">{index + 1}</span>
                 </CardContent>
               </Card>
             </div>
           </CarouselItem>
         ))}
       </CarouselContent>
-      <CarouselPrevious className="leftButton" />
-      <CarouselNext className="rightButton" />
+      <CarouselPrevious className="absolute left-[2rem] top-1/2 transform -translate-y-1/2 z-10" />
+      <CarouselNext className="absolute right-[2rem] top-1/2 transform -translate-y-1/2 z-10" />
     </Carousel>
   );
 }
