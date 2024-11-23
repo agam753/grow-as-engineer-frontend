16310a758d03b3391736695421acab426d4a41b8
Abhinav <gargabhinav0001@gmail.com>
2024-11-23 11:19:50 +0530

    feat: add missing lib and make filter button to right

--- src/components/JobFilters.tsx
+++ src/components/JobFilters.tsx
@@ -20,7 +20,7 @@ const JobFilters = () => {
   return (
     <>
       {/* Mobile View */}
-      <div className="block md:hidden">
+      <div className="block md:hidden flex justify-end">
         <Sheet>
           <SheetTrigger asChild>
             <Button variant="outline">
